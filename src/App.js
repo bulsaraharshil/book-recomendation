876c1f8ce6814c56ec6234e22e82cf11cf8974fa
Harshil Bulsara <39211401+bulsaraharshil@users.noreply.github.com>
2020-12-12 16:23:21 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,78 @@
+import React, { useState } from "react";
+import "./styles.css";
+const bookDatabase = {
+  javascript: [
+    { name: "Eloquent JavaScript", rating: "4/5" },
+    { name: "You Don't Know JS", rating: "3.5/5" }
+  ],
+  css: [
+    { name: "CSS: A Definative Guide", rating: "4/5" },
+    { name: "CSS: The Missing Manual", rating: "3.5/5" }
+  ],
+
+  mythology: [
+    {
+      name: "Sita: The warrior of Mithila",
+      rating: "5/5"
+    },
+    {
+      name: "Jaya",
+      rating: "4.5/5"
+    }
+  ],
+  business: [
+    {
+      name: "Rich Dad Poor Dad",
+      rating: "4.5/5"
+    },
+    {
+      name: "Loonshots",
+      rating: "5/5"
+    }
+  ]
+};
+export default function App() {
+  const [selectedGenre, setGenre] = useState("mythology");
+  function genreClickHandler(genre) {
+    setGenre(genre);
+  }
+  return (
+    <div className="App">
+      <h1>Book Reccomendation</h1>
+      <div>
+        {Object.keys(bookDatabase).map((genre) => (
+          <button
+            onClick={() => genreClickHandler(genre)}
+            style={{
+              cursor: "pointer",
+              borderRadius: "0.5rem",
+              padding: "0.5rem",
+              border: "1px solid",
+              margin: "0.5rem"
+            }}
+          >
+            {genre}
+          </button>
+        ))}
+      </div>
+      <hr />
+      <div>
+        {bookDatabase[selectedGenre].map((book) => (
+          <li
+            key={book.name}
+            style={{
+              listStyle: "none",
+              padding: "0.5rem",
+              border: "1px solid",
+              margin: "1rem",
+              borderRadius: "0.5rem"
+            }}
+          >
+            <div> {book.name} </div>
+            <div> {book.rating} </div>
+          </li>
+        ))}
+      </div>
+    </div>
+  );
+}
